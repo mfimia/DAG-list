c197b6555963d17235dd61bb6bf1e542e02ccf98
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-04 21:42:23 +0100

    local storage for items has been set

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const FORM = document.getElementById("form");
 const INPUT = document.getElementById("input");
+const LIST = JSON.parse(localStorage.getItem("DAG-list")) || [];
 
 // Add event listeners
 FORM.addEventListener("submit", (e) => {
@@ -9,5 +10,11 @@ FORM.addEventListener("submit", (e) => {
 
 const create = (input) => {
   INPUT.value = "";
-  console.log(input);
+  const item = {
+    text: input,
+    id: Math.floor(Math.random() * 100000),
+  };
+  LIST.unshift(item);
+  localStorage.setItem("DAG-list", JSON.stringify(LIST));
+  console.log(LIST);
 };
