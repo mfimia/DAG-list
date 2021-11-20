b2dbb3ee251b0e2256f95b11889756d761a44213
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-20 17:52:46 +0100

    prettier

---
+++ display.js
@@ -0,0 +1,28 @@
+// Function to display items on container
+const display = (loaded = false) => {
+  const box = document.getElementById("display");
+  box.innerHTML = "";
+  LIST.forEach((item, i) => {
+    const span = document.createElement("span");
+    span.innerHTML = `${item.text}`;
+    box.appendChild(span);
+    setAttributes(span, item.id, i, item.color, loaded);
+  });
+};
+
+// Function that takes an item and displays its menu
+const displayMenu = (item, id) => {
+  const dagItem = LIST.filter((element) => element.id == id);
+  if (!dagItem[0].menu) {
+    let optionSize = item.clientWidth > 100 ? 80 : 60;
+    const menu = document.createElement("div");
+    addOptionsMenu(menu);
+    item.clientWidth > 100
+      ? menu.setAttribute("class", "options-menu-large")
+      : menu.setAttribute("class", "options-menu");
+    menu.style.width = `${item.clientWidth + optionSize}px`;
+    menu.style.height = `${item.clientHeight + optionSize}px`;
+    item.appendChild(menu);
+  }
+  displayedMenuActive(id);
+};
